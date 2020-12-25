db727e58c6a337ddc0b4533e35a06ef4ed523866
ashish8082 <ashishyadav063@gmail.com>
2020-12-26 00:49:16 +0530

    service is created

--- TrackingApp/src/app/app-routing.module.ts
+++ TrackingApp/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CountriesComponent } from './components/countries/countries.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+                          { 
+                            path:'',
+                            component:HomeComponent
+                          },
+                          {
+                              path:'countries',component:CountriesComponent
+                          },
+                          {
+                             path:'',
+                             component:NavbarComponent
+                          }
+                        ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
